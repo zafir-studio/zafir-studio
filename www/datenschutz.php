d30b08d786764e7eff510cef6955508f5c16ed5d
zafir-studio <44253944+zafir-studio@users.noreply.github.com>
2018-10-18 11:51:06 +0200

    beta

---
+++ www/datenschutz.php
@@ -0,0 +1,177 @@
+
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <meta name="description" content="ZAFIR Media Studio - Web | Design | Kreativ">
+    <meta name="author" content="ZAFIR Media Studio">
+    <link rel="icon" href="http://psv-steyr.eu/zafir/favicon.ico">
+
+    <title>ZAFIR Media Studio</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://psv-steyr.eu/bootstrap-4.0.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="http://psv-steyr.eu/bootstrap-4.0.0/docs/4.0/examples/carousel/carousel.css" rel="stylesheet">
+    <link href="zafir.css" rel="stylesheet">
+    
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    
+  	<link href="https://fonts.googleapis.com/css?family=Raleway:300,300i,400,400i,500,500i,700,700i,800,800i" rel="stylesheet">
+  	<link href="https://fonts.googleapis.com/css?family=Exo" rel="stylesheet">
+  	
+  	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/zafirAPP.jpg" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/zafirAPP.jpg" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/zafirAPP.jpg" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/zafirAPP.jpg" />
+    
+   <style type="text/css">
+		
+		
+	</style>
+    
+    
+    
+    <script>
+
+    function setCookie(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + "; " + expires;
+    }
+
+    function deleteCookie(cname) {
+        document.cookie = cname + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+        }
+        return "";
+    }
+
+
+   
+    </script>
+    
+    
+  </head>
+  
+  <div id='cookieBar' class="alert alert-dark" style='display:none;background-color:rgba(0,0,0,0.6);color:white;position:fixed;top:0px;width:100%;text-align:center;z-index:2000;'>
+    Wir verwenden Cookies, um unsere Webseite f&uuml;r Sie m&ouml;glichst benutzerfreundlich zu gestalten. Wenn Sie fortfahren, nehmen wir an, dass Sie mit der Verwendung von Cookies auf dieser Webseite einverstanden sind. 
+  	<br><button type="button" class="btn btn-primary" style='margin-top:5px;'onclick='setCookieACC();'>Einverstanden</button>
+  </div>
+  
+  <body style="background-color:#222;margin:0;padding:0;width:100%;">
+
+
+    <main role="main" style="background-color:transparent;">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+
+	  
+	  <div class="container" style="background-color:transparent;height:0px;z-index:-1;"></div>
+	  
+	      <div id='myServices' class="jumbotron firstJumbo" style="border-top:1px solid #444;width:100%;min-height:90vh;background-color:#eff5ff;margin-left:0;margin-right:0;text-align:center;margin-bottom:0px;border-radius:0;">
+	        <div class="container">
+	        
+        	<h2 class="featurette-heading" style='margin-top:30px;color:black;margin-bottom:20px;'><span style='color:black;font-weight:bold;'>Datenschutz</span></h2>
+	  		<p class='infoText' style='margin-top:2rem;font-family:"Raleway",cursive;'>
+	  			<b>ZAFIR Media Studio</b>
+	  		</p>
+	  		<p class='infoText' style='margin-top:1rem;font-family:"Raleway",cursive;font-size:1rem;'>
+	  			Daniel Tochenhagen<br><a href="mailto:office@zafir-studio.eu">office@zafir-studio.eu</a>
+	  		</p>
+	        <br><br>
+	        
+	          <p class='infoText' style='color:#444;margin-top:2rem;'><b>Datenschutzerkl&auml;rung</b></p>
+	          <p class='infoText' style='color:#444;'>Der Schutz Ihrer pers&ouml;nlichen Daten ist uns ein besonderes Anliegen. Wir verarbeiten Ihre Daten daher ausschlie&szlig;lich auf Grundlage der gesetzlichen Bestimmungen (DSGVO, TKG 2003). In diesen Datenschutzinformationen informieren wir Sie &uuml;ber die wichtigsten Aspekte der Datenverarbeitung im Rahmen unserer Website. Sofern innerhalb des Internetangebotes die M&ouml;glichkeit zur Eingabe pers&ouml;nlicher oder gesch&auml;ftlicher Daten (E-Mail-Adressen, Namen, Anschriften) besteht, so erfolgt die Preisgabe dieser Daten seitens des Nutzers auf ausdr&uuml;cklich freiwilliger Basis. Die Inanspruchnahme und Bezahlung aller angebotenen Dienste ist - soweit technisch m&ouml;glich und zumutbar - auch ohne Angabe solcher Daten bzw. unter Angabe anonymisierter Daten oder eines Pseudonyms gestattet. Die Nutzung der im Rahmen des Impressums oder vergleichbarer Angaben ver&ouml;ffentlichten Kontaktdaten wie Postanschriften, Telefon- und Faxnummern sowie E-Mail-Adressen durch Dritte zur &Uuml;bersendung von nicht ausdr&uuml;cklich angeforderten Informationen ist nicht gestattet. Rechtliche Schritte gegen die Versender von so genannten Spam-Mails bei Verst&ouml;&szlig;en gegen dieses Verbot sind ausdr&uuml;cklich vorbehalten. Grunds&auml;tzlich k&ouml;nnen Sie unsere Website ohne die Angabe personenbezogener Daten besuchen. Wir erfahren den Namen Ihres Providers, die Website, von der Sie auf unser Informationsangebot gekommen sind, und die Seiten, die Sie bei uns aufgerufen haben. Diese Daten werden von uns statistisch ausgewertet und dienen zur Optimierung unserer Services, die wir f&uuml;r Sie anbieten.</p>
+	          
+	          <p class='infoText' style='color:#444;margin-top:2rem;'><b>Verwendung von Google Analytics</b></p>
+	          <p class='infoText' style='color:#444;'>Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. ("Google") Google Analytics verwendet sog. "Cookies", Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie erm&ouml;glicht. Die durch den Cookie erzeugten Informationen &uuml;ber Ihre Benutzung diese Website (einschlie&szlig;lich Ihrer IP-Adresse) wird an einen Server von Google in den USA &uuml;bertragen und dort gespeichert. Google wird diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports &uuml;ber die Websiteaktivit&auml;ten f&uuml;r die Websitebetreiber zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen zu erbringen. Auch wird Google diese Informationen gegebenenfalls an Dritte &uuml;bertragen, sofern dies gesetzlich vorgeschrieben oder soweit Dritte diese Daten im Auftrag von Google verarbeiten. Google wird in keinem Fall Ihre IP-Adresse mit anderen Daten der Google in Verbindung bringen.
+					<br><br>Sie k&ouml;nnen die Speicherung der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern; dar&uuml;ber hinaus k&ouml;nnen Sie die Erfassung der durch das Cookie erzeugten Daten sowie die Verarbeitung dieser Daten durch Google verhindern, indem Sie das unter dem folgenden Link verf&uuml;gbare Browser-Plugin herunterladen und installieren: http://tools.google.com. Wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht s&auml;mtliche Funktionen dieser Website voll umf&auml;nglich nutzen k&ouml;nnen.
+					<br><br>Durch die Nutzung dieser Website erkl&auml;ren Sie sich mit der Bearbeitung der &uuml;ber Sie erhobenen Daten durch Google in der zuvor beschriebenen Art und Weise und zu dem zuvor benannten Zweck einverstanden.
+			  </p>
+	          
+	          <p class='infoText' style='color:#444;margin-top:2rem;'><b>Google Map</b></p>
+	          <p class='infoText' style='color:#444;'>Diese Webseite verwendet das Produkt Google Maps von Google Inc. Durch Nutzung dieser Webseite erkl&auml;ren Sie sich mit der Erfassung, Bearbeitung sowie Nutzung der automatisiert erhobenen Daten durch Google Inc, deren Vertreter sowie Dritter einverstanden. Die Nutzungsbedingungen von Google Maps finden sie unter "Nutzungsbedingungen von Google Maps". </p>
+	          
+	          <p class='infoText' style='color:#444;margin-top:2rem;'><b>Cookies</b></p>
+	          <p class='infoText' style='color:#444;'>Insbesondere im Hinblick auf die Verwendung von Cookies. Der Website-Betreiber weist darauf hin, dass auf dieser Website Cookies verwendet werden. Cookies sind Textdateien, die auf der Festplatte des Nutzers hinterlegt werden, um den jeweiligen Nutzer beim wiederholten Besuch auf der Website oder auf einer anderen Website des Partnernetzwerkes, identifizieren zu k&ouml;nnen.
+					<br><br>Sie k&ouml;nnen die Installation der Cookies durch eine entsprechende Einstellung Ihrer Browser Software verhindern. Wir weisen Sie jedoch darauf hin dass Sie bei einer Verhinderung von Cookies gegebenenfalls nicht s&auml;mtliche Funktionen der Website nutzen k&ouml;nnen.
+					<br><br>Mit Nutzung der Website ohne vorherige Ablehnung von Cookies &uuml;ber Ihre Browsereinstellung erkl&auml;ren Sie sich mit der Verwendung von Cookies und der daraus gewonnen Informationen zu dem beschriebenen Zweck einverstanden. Ihre Zustimmung ist jederzeit durch &Auml;nderung Ihrer Browsereinstellungen widerrufbar; mit &Auml;nderung der Browsereinstellungen werden keine Cookies mehr gesetzt.
+			  </p>
+						          
+	          <p class='infoText' style='color:#444;margin-top:2rem;'><b>Auskunft, L&ouml;schung, Sperrung</b></p>
+	          <p class='infoText' style='color:#444;'>Ihnen stehen grunds&auml;tzlich die Rechte auf Auskunft, Berichtigung, L&ouml;schung, Einschr&auml;nkung, Daten&uuml;bertragbarkeit, Widerruf und Widerspruch zu. Wenn Sie glauben, dass die Verarbeitung Ihrer Daten gegen das Datenschutzrecht verst&ouml;&szlig;t oder Ihre datenschutzrechtlichen Anspr&uuml;che sonst in einer Weise verletzt worden sind, k&ouml;nnen Sie sich bei der Aufsichtsbeh&ouml;rde beschweren. In &Ouml;sterreich ist dies die Datenschutzbeh&ouml;rde.
+					<br>Sonst kontaktieren Sie uns einfach und wir helfen gerne weiter!
+			  </p>
+	          
+	        </div>
+	      </div>
+	      
+	      
+	      
+	      
+	      
+      </div> <!-- /container -->
+
+		<!-- FOOTER -->
+		      <footer class="container" style='max-width:100%;padding:20px 1% 10px 1%;background-color:#eff5ff;'>
+		        <p>&copy; 2018 - Zafir Media Studio &middot; Designed by <a href="https://www.linkedin.com/in/tochenhagen">Daniel Tochenhagen</a> &middot; <a href="datenschutz.php">Datenschutz</a></p>
+		      </footer>
+      
+
+    </main>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+	    <script src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
+	    <script>window.jQuery || document.write('<script src="bootstrap-4.0.0/assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+	    <script src="bootstrap-4.0.0/assets/js/vendor/popper.min.js"></script>
+	    <script src="bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
+	    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+	    <script src="bootstrap-4.0.0/assets/js/vendor/holder.min.js"></script>
+  </body>
+
+
+</html>
+
+
+
+<script>
+
+function setCookieACC(){
+	setCookie('cookieCB', '1', 1);
+	$('#cookieBar').css("display","none");
+}
+
+if(getCookie('cookieCB') != '1'){
+	$('#cookieBar').css("display","block");
+}
+
+
+$(function(){ 
+    var navMain = $(".navbar-collapse"); // avoid dependency on #id
+    // "a:not([data-toggle])" - to avoid issues caused
+    // when you have dropdown inside navbar
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+});
+
+
+</script>
+
+
+
---
+++ www/wop.php
@@ -0,0 +1,82 @@
+<body style='margin-bottom:70px;'>
+
+<link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet" />
+<div style="display:table;border-spacing:0.5em;width:100%;margin-bottom:30px;">
+<div style="display:table-row;">
+<div onclick="getSection(0)" style="display:table-cell;width:33%;height:100px;cursor:pointer;">
+<div style="height:100px;width:100px;margin-left: auto;margin-right: auto;"><img src="http://psv-steyr.eu/zafir/img/wopSnake.jpg" style="border:2px solid white;border-radius:50%;height:100px;width:100px;" />
+<div style="text-align:center;font-family: &quot;Tangerine&quot;, cursive;font-size:30px;color:darkred;margin-top:10px;font-weight:bold;">Story</div>
+</div>
+</div>
+
+<div onclick="getSection(1)" style="display:table-cell;width:33%;height:100px;cursor:pointer;">
+<div style="height:100px;width:100px;margin-left: auto;margin-right: auto;"><img src="http://psv-steyr.eu/zafir/img/wopDiary.jpg" style="border:2px solid white;border-radius:50%;height:100px;width:100px;" />
+<div style="text-align:center;font-family: &quot;Tangerine&quot;, cursive;font-size:30px;color:darkred;margin-top:10px;font-weight:bold;">Tagebuch</div>
+</div>
+</div>
+
+<div onclick="getSection(2)" style="display:table-cell;width:33%;height:100px;cursor:pointer;">
+<div style="height:100px;width:100px;margin-left: auto;margin-right: auto;"><img src="http://psv-steyr.eu/zafir/img/wopWand.jpg" style="border:2px solid white;border-radius:50%;height:100px;width:100px;" />
+<div style="text-align:center;font-family: &quot;Tangerine&quot;, cursive;font-size:30px;color:darkred;margin-top:10px;font-weight:bold;">Charackter</div>
+</div>
+</div>
+</div>
+</div>
+
+<div id="myStory" style="display:none;border-spacing:0.5em;width:100%;margin-top:40px;margin-bottom:30px;">
+<div style="font-family: &quot;Cinzel&quot;, serif;font-size:30px;margin-bottom:30px;">Der Weg durch das ewige Eis...</div>
+
+<div style="font-family: &quot;Satisfy&quot;, cursive;font-size:20px;"><b>Dany Zafir, in den Schluchten des Hochgebirges umringt vom ewigen Eis Sibiriens geboren, ist der Sohn einer Hexe und eines Muggels.</b><br />
+<br />
+Gezeichnet von den anarchischen Zust&auml;nden und dem zur&uuml;ckgebliebenem Gedankengut der Leute in ihrer Heimat, fl&uuml;chtete Stella Zafir mit ihrem zweij&auml;hrigen Kind nach Mitteleuropa. Doch willensstark und aufopferungsvoll wie es f&uuml;r ihre Familie typisch war schaffte sie es, f&uuml;r sich und ihren Sohn ein neues Leben zu schaffen.<br />
+<br />
+Aufgewachsen in einer kleinen Stadt in &Ouml;sterreich konnte Dany zufrieden und ohne Sorgen aufwachsen. Obwohl gerade das n&ouml;tigste zum Leben da war, war es genug um gl&uuml;cklich zu sein. Trotz manch magischer Ausbr&uuml;che von Dany hielt seine Mutter ihn gro&szlig;teils von der Magie fern, um sie beide von einer weiteren Ausgrenzung zu sch&uuml;tzen.<br />
+<br />
+Lange blieb das Talent des Jungen unbemerkt. Erst ein enger Freund der Familie erkannte schlie&szlig;lich das unbewusst auflodernde Verlangen von Dany sein Potenzial zu entfalten. Irgendetwas in seinem Inneren wollte nicht l&auml;nger unterdr&uuml;ckt werden. Ausserdem stand ein neues Jahr in Hogwarts vor der T&uuml;r und Dany war nun alt genug. Der Brief w&uuml;rde sowieso kommen. Somit musste Stella die Wahrheit ans Licht bringen und Dany seiner wahren Bestimmung n&auml;her bringen...<br />
+<br />
+&quot;<i>... nun musst du deinen Weg w&auml;hlen Liebling. Die magische Welt birgt nicht nur Gefahren, du wirst auch viel Gl&uuml;ck darin finden. Meine Entscheidung war es, uns vor den falschen Menschen zu sch&uuml;tzen. Deine Entscheidung ist es nun, sich mit den richtigen einzulassen...&quot;</i><br />
+<br />
+<b>Ein paar Monate sp&auml;ter...</b><br />
+<br />
+<span style="font-size:30px;color:darkgreen;">&quot;Slytherin!&quot;</span></div>
+</div>
+
+<div id="myDiary" style="display:none;border-spacing:0.5em;width:100%;margin-top:40px;margin-bottom:30px;">
+<div style="font-family: &quot;Cinzel&quot;, serif;font-size:30px;margin-bottom:30px;">Auszug aus den t&auml;glichen Erlebnissen</div>
+
+<div style="font-family: &quot;Satisfy&quot;, cursive;font-size:20px;margin-bottom:30px;"><b>Tagebucheintrag #1:</b> erster Schultag<br />
+<br />
+Hogwarts. Ich kann es immer noch kaum fassen. Ich bin Mutter so dankbar, dass sie mir das alles erm&ouml;glicht. Der Umzug in die Highlands von Schottland war f&uuml;r uns alles andere als mit Spa&szlig; verbunden. Aber Mutter geht es hier gut. Die magische Welt ist einfach der Hammer. Schon die Winkelgasse hat mich fasziniert, als wir meine Schulsachen gekauft haben. Und das Schloss ist einfach gro&szlig;artig.<br />
+Ich wurde vom sprechenden Hut nach Slytherin gesteckt. Ich f&uuml;hl mich hier jetzt schon sehr wohl und freu mich auf weitere Bekanntschaften. Die ersten Unterrichtsstunden waren gepr&auml;gt von Vorstellungsrunden. Ich kann es kaum erwarten meinen neuen Zauberstab zu schwingen und das theoretisch erlernte in der Praxis um zu setzen.</div>
+</div>
+
+<div id="myChar" style="display:none;border-spacing:0.5em;width:100%;margin-top:40px;margin-bottom:30px;">
+<div style="font-family: &quot;Cinzel&quot;, serif;font-size:30px;font-size:30px;">Dany Zafir</div>
+</div>
+
+
+
+<script>
+ function getSection(id){
+ 	switch(id){
+    	case 0:
+        	document.getElementById('myStory').style.display = 'table';
+        	document.getElementById('myDiary').style.display = 'none';
+        	document.getElementById('myChar').style.display = 'none';
+        	break;
+    	case 1:
+        	document.getElementById('myStory').style.display = 'none';
+        	document.getElementById('myDiary').style.display = 'table';
+        	document.getElementById('myChar').style.display = 'none';
+        	break;
+    	case 2:
+        	document.getElementById('myStory').style.display = 'none';
+        	document.getElementById('myDiary').style.display = 'none';
+        	document.getElementById('myChar').style.display = 'table';
+    }
+ }
+</script>
+
+</body>
